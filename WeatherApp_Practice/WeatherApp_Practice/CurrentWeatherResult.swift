71c925dcc66d38d7fab9d4de85f6d04f63c60773
youngjin <bryjna07@nate.com>
2024-12-06 10:10:37 +0900

    feat: 날씨앱 시작 초기세팅

---
+++ WeatherApp_Practice/WeatherApp_Practice/CurrentWeatherResult.swift
@@ -0,0 +1,8 @@
+//
+//  CurrentWeatherResult.swift
+//  WeatherApp_Practice
+//
+//  Created by t2023-m0033 on 12/6/24.
+//
+
+import Foundation
---
+++ WeatherApp_Practice/WeatherApp_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp_Practice
+//
+//  Created by t2023-m0033 on 12/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+  
+    }
+
+
+}
+
